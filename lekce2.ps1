f96c12ecdbabe685dc10326691411d7225543e50
Lubos87CZ <moudry.l@gmail.com>
2026-06-11 20:08:41 +0200

    lekce 2 pokracovani 11.6.

--- lekce2.ps1
+++ lekce2.ps1
@@ -9,3 +9,15 @@ Get-Process | Sort-Object CPU -Descending | Select-Object -First 10 | Format-Tab
 #zobraz informace o aktualne spustenych procesech, seradi je podle pouziti CPU sestupne, zobrazi pouze prvnich 10 procesu, zobrazi pouze jmeno a pouziti CPU a ulozi do souboru processes.txt v aktualnim adresari
 Get-Content ".\processes.txt"
 #zobraz obsah souboru processes.txt, ktery jsme vytvorili v predchozim prikazu
+$psobj = new-object psobject
+$seznamProcesu = Get-Process | Select-Object id, processname, CPU |Sort-Object ID | Select-Object -First 15
+#vytvori novy objekt typu PSObject, ziska seznam aktualne spustenych procesu a ulozi ho do promenne $seznamProcesu
+$psobj | Add-Member -MemberType NoteProperty -Name "BeziciProcesy" -Value $seznamProcesu
+$psobj.BeziciProcesy   | Format-Table -AutoSize 
+#volam primo konkretni vlastnost objektu $psobj.BeziciProcesy, ktera obsahuje seznam procesu, a zobrazuji tento seznam ve formatu tabulky s automatickou velikosti sloupcu
+#pridá tento seznam jako vlastnost "Bezici procesy" do objektu $psobj a zobrazi obsah objektu ve formatu tabulky
+"Vypis objektu PSObject pomoci pipe-line"
+$psobj | Select-Object -ExpandProperty BeziciProcesy
+#zobraz obsah vlastnosti "BeziciProcesy" objektu $psobj pomoci pipe-line, ktera obsahuje seznam procesu
+#parametr -Property vraci puvodni objekt s jednou vlastnosti. V konzoli bude zobrazen jen jeden radek se slozenymi zavorkami
+#parametr -ExpandProperty zahodi hlavni objekt a zobrazi pouze obsah vlastnosti "BeziciProcesy", ktery je seznamem procesu, a zobrazi tento seznam jako tabulku
